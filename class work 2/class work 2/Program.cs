321c02dd790a68cd2e5aa89da47dbc541fe1ddbf
Yshk92 <falconblaze55@gmail.com>
2022-06-27 03:18:43 +0300

    this is class work

---
+++ class work 2/class work 2/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string heroname = " teo ";
+int heropower = 1120;
+string vilianname = "lilith";
+int vilianpower = 100;
+
+if (vilianpower > heropower)
+{
+    Console.WriteLine(" the strongest is " + vilianname);
+}
+
+else if (vilianpower < heropower)
+{
+    Console.WriteLine("the strongest is " + heroname);
+}
+
+ else
+{
+    Console.WriteLine("they are equel");
+}
---
+++ class work 3/class work 3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+float playerspeed = 2;
+Console.WriteLine(playerspeed);
+void setspeed () { 
+
+    playerspeed = 2.5f;
+ Console.WriteLine(playerspeed); 
+}
+setspeed ();
+
+setspeed();
+    Console.WriteLine(playerspeed);
+
